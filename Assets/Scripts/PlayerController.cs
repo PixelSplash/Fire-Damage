967a1e5311223c09e40d134db34b6568eb6db947
Briam David Mora Villalobos <briamtec@gmail.com>
2021-10-26 11:45:33 -0600

    primer  commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    public float speed = 1;
+    public float rotationSpeed = 1;
+    public float jumpForce = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Se obtiene el rigidbody para mover el jugador
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Saltar
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Aplicalo la fuerza de salto a la velocidad en y del rigidbody
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y + jumpForce, rigidBody.velocity.z);
+        }
+
+        // Moverse hacia adelante y atras
+        // Obtengo la direccion a la que el jugador quiere moverse. -1 si quiero ir hacia atras o 1 si quiero ir hacia adelante
+        float vertical = Input.GetAxis("Vertical");
+
+        // Le doy la velocidad correspondiente al rigidbody para moverse en esta direccion
+        rigidBody.velocity = transform.forward * speed * vertical + new Vector3(0, rigidBody.velocity.y, 0);
+
+        // Girar hacia la derecha e izquierda
+        // Obtengo la direccion a la que el jugador quiere moverse. -1 si quiero ir hacia la izquierda o 1 si quiero ir hacia derecha
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Rotate(new Vector3( 0, rotationSpeed * horizontal * Time.deltaTime, 0));
+
+    }
+}
---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+
+    bool chasingPlayer = false;
+    public Transform player;
+    public float speed = 1;
+    public float pushForce = 1;
+    Rigidbody rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Si el tanque esta persiguiendo al jugador, que siempre vuelva a ver hacia el y se mueva hacia adelante
+        if (chasingPlayer)
+        {
+            // Rota al enemigo para que vea hacia el jugador
+            transform.LookAt(player.position);
+            // Mueve hacia el frente al enemigo
+            rig.velocity = transform.forward * speed + new Vector3(0, rig.velocity.y, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            chasingPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            chasingPlayer = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Con los tags se puede saber con lo que estoy chocando
+        if (collision.gameObject.tag == "Player")
+        {
+            Rigidbody playerRig = collision.gameObject.transform.GetComponent<Rigidbody>();
+            playerRig.velocity = transform.forward * pushForce + new Vector3(0, playerRig.velocity.y, 0);
+        }
+    }
+}
